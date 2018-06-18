9d91f830e2bc37c4ed92c1287a6b7e31d5f6d02b
Jerry Wu <wujerry573@gmail.com>
2018-06-18 17:08:33 -0500

    suck on vegplot

--- seed.js
+++ seed.js
@@ -1,16 +1,60 @@
-const {db} = require('./models');
-const {VEGETABLE, PLOT, GARDENER} = require('./models');
+const { db } = require('./models');
+const { VEGETABLE, PLOT, GARDENER } = require('./models');
+
+const vegPlot = db.model('VegetablePlots');
 
 VEGETABLE.create({
 	name: 'eggplant',
 	color: 'purple',
 	plant_on: Date.now()
-}).then((veg) => {
-	console.log("this is a: " ,veg);
-}).catch(err => {
-	console.error(err);
-});
+})
+	.then(veg => {
+		return GARDENER.create({
+			name: 'jacoby',
+			age: '20',
+			favoriteVegetableId: veg.id
+		});
+	})
+	.then(plotveg => {
+		return PLOT.create({
+			size: '1',
+			shaded: 'true',
+			GARDENERId: plotveg.id
+		});
+  }).then(veg() => {
+    return vegPlot.create({
+      VEGETABLEId: veg.id,
+      PLOTId:
+    })
+  }
+  )
+	.catch(err => {
+		console.error(err);
+	});
 
+
+// PLOT.create({
+// 	size: '1',
+// 	shaded: 'true'
+// })
+// 	.then(plot => {
+// 		return plot;
+// 	})
+// 	.catch(err => {
+// 		console.error(err);
+// 	});
+
+// GARDENER.create({
+// 	name: 'jacoby',
+// 	age: '20'
+// })
+//   .then(human => {
+//     return human;
+// 		// return PLOT.create({ humanName: human.name });
+// 	})
+// 	.catch(err => {
+// 		console.error(err);
+// 	});
 
 db.sync()
 	.then(() => {
